026ad67214f8747fbe91c7351d3b1238db1a80a0
logicmyth <staley77@protonmail.com>
2018-02-15 11:53:55 -0500

    add ui logic/styling/new algorithm

--- scripts/events.js
+++ scripts/events.js
@@ -1,5 +1,5 @@
 'use strict';
-window.onload=function () {alert("doc loaded")};
+
 const onShuffle = () => {
   var one = Math.floor(Math.random() * 101;
   var two = Math.floor(Math.random() * 101;
@@ -12,33 +12,30 @@ const onShuffle = () => {
   var nine = Math.floor(Math.random() * 101;
   var ten = Math.floor(Math.random() * 101;
 
-  console.log("shuffling");
   var arr = [];
   arr.push(one,two,three,four,five,six,seven,eight,nine,ten)
   return arr
 }.done(ui.shuffleSuccess);
 
-const onSort = () => {
-  console.log("sorting");
+var i = 0;
+var j = onShuffle().length -1;
+
+const bubbleStep = (arr,i,j,swapped) => {
   var arr = onShuffle();
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) {
-      var a = arr[i + 1]
-      var b = arr[i]
-      arr[i] = b
-      arr[i + 1] = a
-    }
+  if (arr[i] > arr[i+1]) {
+    var x = arr[i];
+    var y = arr[i+1];
+    arr[i] = y;
+    arr[i+1] = x;
+    swapped = true;
+  }
+  i++;
+  if (i===j) {
+    j--;
+  }
   }
   return arr;
 }.done(ui.sortSuccess);
 
  $('.sort').on('click', onSort);
  $('.shuffle').on('click', onShuffle);
-
-
-
-
-//sorting alg while loop add conditional logic
-//list of functions 1. gen randomnumber function 2. run bubbleSort on array being fed func updates ui function respond to button command
-//bubble func steps js grabs current order puts back in bubble sort
-//add a way in loop to stop after one interation
--- scripts/ui.js
+++ scripts/ui.js
@@ -11,18 +11,278 @@ $('.seven').html(arr[6]);
 $('.eight').html(arr[7]);
 $('.nine').html(arr[8]);
 $('.ten').html(arr[9]);
-}
+}.then(function(){
+  if ($('.one').html() < 10) {
+    $('.one').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.one').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.one').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.one').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.one').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.one').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.one').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.one').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.one').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.one').addClass('ninetyToHundo')
+  } else {
+    $('.one').html(arr[0])
+  }
+}).then(function(){
+  if ($('.two').html() < 10) {
+    $('.two').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.two').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.two').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.two').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.two').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.two').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.two').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.two').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.two').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.two').addClass('ninetyToHundo')
+  } else {
+    $('.two').html(arr[1])
+  }
+}).then(function(){
+  if ($('.three').html() < 10) {
+    $('.three').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.three').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.three').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.three').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.three').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.three').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.three').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.three').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.three').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.three').addClass('ninetyToHundo')
+  } else {
+    $('.three').html(arr[2])
+  }
+}).then(function(){
+  if ($('.four').html() < 10) {
+    $('.four').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.four').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.four').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.four').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.four').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.four').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.four').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.four').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.four').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.four').addClass('ninetyToHundo')
+  } else {
+    $('.four').html(arr[3])
+  }
+}).then(function(){
+  if ($('.five').html() < 10) {
+    $('.five').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.five').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.five').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.five').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.five').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.five').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.five').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.five').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.five').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.five').addClass('ninetyToHundo')
+  } else {
+    $('.five').html(arr[4])
+  }
+}).then(function(){
+  if ($('.six').html() < 10) {
+    $('.six').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.six').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.six').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.six').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.six').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.six').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.six').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.six').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.six').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.six').addClass('ninetyToHundo')
+  } else {
+    $('.six').html(arr[5])
+  }
+}).then(function(){
+  if ($('.seven').html() < 10) {
+    $('.seven').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.seven').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.seven').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.seven').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.seven').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.seven').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.seven').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.seven').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.seven').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.seven').addClass('ninetyToHundo')
+  } else {
+    $('.seven').html(arr[6])
+  }
+}).then(function(){
+  if ($('.eight').html() < 10) {
+    $('.eight').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.eight').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.eight').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.eight').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.eight').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.eight').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.eight').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.eight').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.eight').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.eight').addClass('ninetyToHundo')
+  } else {
+    $('.eight').html(arr[7])
+  }
+}).then(function(){
+  if ($('.nine').html() < 10) {
+    $('.nine').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.nine').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.nine').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.nine').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.nine').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.nine').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.nine').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.nine').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.nine').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.nine').addClass('ninetyToHundo')
+  } else {
+    $('.nine').html(arr[8])
+  }
+}).done(function(){
+  if ($('.ten').html() < 10) {
+    $('.ten').addClass('zeroToTen')
+  } else if ($('.one').html() >10 && $('.one').html() < 20) {
+    $('.ten').addClass('tenToTwenty')
+  } else if ($('.one').html() >20 && $('.one').html() < 30) {
+    $('.ten').addClass('twentyToThirty')
+  } else if ($('.one').html() >30 && $('.one').html() < 40) {
+    $('.ten').addClass('thirtyToForty')
+  } else if ($('.one').html() >40 && $('.one').html() < 50) {
+    $('.ten').addClass('fortyToFifty')
+  } else if ($('.one').html() >50 && $('.one').html() < 60) {
+    $('.ten').addClass('fiftyToSixty')
+  } else if ($('.one').html() >60 && $('.one').html() < 70) {
+    $('.ten').addClass('sixtyToSeventy')
+  } else if ($('.one').html() >70 && $('.one').html() < 80) {
+    $('.ten').addClass('seventyToEighty')
+  } else if ($('.one').html() >80 && $('.one').html() < 90) {
+    $('.ten').addClass('eightyToNinety')
+  } else if ($('.one').html() >90 && $('.one').html() < 100) {
+    $('.ten').addClass('ninetyToHundo')
+  } else {
+    $('.ten').html(arr[9])
+  }
+});
 
 const sortSuccess (arr) => {
-  $('.one').html(arr[0]);
-  $('.two').html(arr[1]);
-  $('.three').html(arr[2]);
-  $('.four').html(arr[3]);
-  $('.five').html(arr[4]);
-  $('.six').html(arr[5]);
-  $('.seven').html(arr[6]);
-  $('.eight').html(arr[7]);
-  $('.nine').html(arr[8]);
-  $('.ten').html(arr[9]);
-  $('.1').html(arr[0]);
-}
+  if ($('.one').html() > $('.two').html()) {
+    $('.one').html($('.two').text())
+  } else if (('.two').html() > $('.three').html()) {
+    $('.two').html($('.three').text())
+  } else if (('.three').html() > $('.four').html()) {
+    $('.three').html($('.four').text())
+  } else if (('.four').html() > $('.five').html()) {
+    $('.four').html($('.five').text())
+  } else if (('.five').html() > $('.six').html()) {
+    $('.five').html($('.six').text())
+  } else if (('.six').html() > $('.seven').html()) {
+    $('.six').html($('.seven').text())
+  } else if (('.seven').html() > $('.eight').html()) {
+    $('.seven').html($('.eight').text())
+  } else if (('.eight').html() > $('.nine').html()) {
+    $('.eight').html($('.nine').text())
+  } else if (('.nine').html() > $('.ten').html()) {
+    $('.nine').html($('.ten').text())
+  } else {
+    alert("sorted!");
+  };
+
+  // $('.one').html(arr[0]);
+  // $('.two').html(arr[1]);
+  // $('.three').html(arr[2]);
+  // $('.four').html(arr[3]);
+  // $('.five').html(arr[4]);
+  // $('.six').html(arr[six  $('.seven').html(arr[6]);
+  // $('.eight').html(arr[7]);
+  // $('.nine').html(arr[8]);
+  // $('.ten').html(arr[9]);
+};
